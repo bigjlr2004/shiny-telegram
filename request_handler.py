b3b157488aca749286d80b79b66d3572e962fda1
John Roberts <bigjlr2004@yahoo.com>
2024-09-26 19:07:13 -0500

    Windows Kennel Repository

---
+++ request_handler.py
@@ -0,0 +1,171 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from views import (
+    get_all_animals,
+    get_single_animal,
+    create_animal,
+    delete_animal,
+    get_all_locations,
+    get_single_location,
+    create_location,
+    delete_location,
+    get_single_employee,
+    get_all_employees,
+    create_employee,
+    delete_employee,
+    get_all_customers,
+    get_single_customer,
+    create_customer,
+    delete_customer,
+)
+import json
+
+# Here's a class. It inherits from another class.
+# For now think ofa class as a container for functions that
+# work together for a common purpose. In this case, that
+# common purpose is to respond to HTTP requests from a client.
+
+
+class HandleRequests(BaseHTTPRequestHandler):
+    # This is a docstring it should be at the beginning if all classes and functions.
+    # IT gives a description of the class or function.
+    """Controls the functionality of any GET, PUT, POST, DELETE requests to the server"""
+
+    def parse_url(self, path):
+        # Just like splitting a string in JavaScript. If the
+        # path is "/animals/1", the resulting list will
+        # have "" at index 0, "animals" at index 1, and "1"
+        # at index 2.
+        path_params = path.split("/")
+        resource = path_params[1]
+        id = None
+
+        # Try to get the item at index 2
+        try:
+            # Convert the string "1" to the integer 1
+            # This is the new parseInt()
+            id = int(path_params[2])
+        except IndexError:
+            pass  # No route parameter exists: /animals
+        except ValueError:
+            pass  # Request had trailing slash: /animals/
+
+        return (resource, id)  # This is a tuple
+
+    # Here's a class function
+    def _set_headers(self, status):
+        # Notice this DocString also includes information about the arguments passed to the function
+        """Sets the status code, Content-Type and Access-Control-Allow-Origin
+        headers on the response
+
+        Args:
+            status(number): the status code to return to the front end"""
+        self.send_response(status)
+        self.send_header("Content-type", "application-json")
+        self.send_header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+        self.send_header(
+            "Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept"
+        )
+        self.end_headers()
+
+    # Here's a method on the class that overrides the parent's method.
+    # It handles any GET request.
+    def do_GET(self):
+        """Handles GET requests to the server"""
+
+        # Set the response code to OK
+        self._set_headers(200)
+        response = {}  # Default response
+
+        # Parse the URL and capture the tuple that is returned
+        (resource, id) = self.parse_url(self.path)
+
+        # It's an if else statement
+        if resource == "animals":
+            if id is not None:
+                response = get_single_animal(id)
+            else:
+                response = get_all_animals()
+        elif resource == "locations":
+            if id is not None:
+                response = get_single_location(id)
+            else:
+                response = get_all_locations()
+        elif resource == "employees":
+            if id is not None:
+                response = get_single_employee(id)
+            else:
+                response = get_all_employees()
+        elif resource == "customers":
+            if id is not None:
+                response = get_single_customer(id)
+            else:
+                response = get_all_customers()
+        # This weird code sends a response back to the client
+        self.wfile.write(json.dumps(response).encode())
+
+        # Here's a method on the class that overrides the parent's default
+        # It handles any POST Request
+
+    def do_POST(self):
+        """Handles POST requests to the server"""
+        self._set_headers(201)
+
+        content_len = int(self.headers.get("content-length", 0))
+        post_body = self.rfile.read(content_len)
+
+        # Convert JSON string to a python dictionary
+        post_body = json.loads(post_body)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Initialize new object to return
+        new_object = None
+
+        # Add anew animal to the list. Don't worry about the orange squiggle
+        # you'll define the create animal function next.
+        if resource == "animals":
+            new_object = create_animal(post_body)
+        elif resource == "locations":
+            new_object = create_location(post_body)
+        elif resource == "employees":
+            new_object = create_employee(post_body)
+        elif resource == "customers":
+            new_object = create_customer(post_body)
+
+        # Encode the new animal and send in response
+        self.wfile.write(json.dumps(new_object).encode())
+
+    def do_DELETE(self):
+        # Set a 204 Response Code
+        self._set_headers(204)
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Delete a single animal from the list
+        if resource == "animals":
+            delete_animal(id)
+        elif resource == "employees":
+            delete_employee(id)
+        elif resource == "customers":
+            delete_customer(id)
+        elif resource == "locations":
+            delete_location(id)
+        self.wfile.write("".encode())
+
+    # Here's a method on the class that overrides the parent's method
+    # It handles any PUT request
+    # def do_PUT(self):
+    #     """Handles PUT requests to the server"""
+    #     self.do_POST()
+
+
+def main():
+    """Starts the server on port 8080 using the HandleRequests class"""
+    host = ""
+    port = 8088
+    HTTPServer((host, port), HandleRequests).serve_forever()
+
+
+if __name__ == "__main__":
+    main()
---
+++ views/animal_requests.py
@@ -0,0 +1,48 @@
+ANIMALS = [
+    {"id": 1, "name": "Snickers", "species": "Dog", "locationId": 1, "customerId": 4},
+    {"id": 2, "name": "Roman", "species": "Dog", "locationId": 1, "customerId": 2},
+    {"id": 3, "name": "Blue", "species": "Cat", "locationId": 2, "customerId": 1},
+]
+
+
+def get_all_animals():
+    return ANIMALS
+
+
+def get_single_animal(id):
+    requested_animal = None
+    for animal in ANIMALS:
+        if animal["id"] == id:
+            requested_animal = animal
+    return requested_animal
+
+
+def create_animal(animal):
+    # Get the id value of the last animal in the list
+    max_id = ANIMALS[-1]["id"]
+    # Add 1 to whatever that number is
+    new_id = max_id + 1
+
+    # Add an 'id' property to the animal dictionary
+    animal["id"] = new_id
+
+    # Add the animal dictionary to the list
+    ANIMALS.append(animal)
+
+    # Return the dictionary with 'id' property added
+    return animal
+
+
+def delete_animal(id):
+    # Initialize animal_index as -1 in case one isn't found
+    animal_index = -1
+
+    # Iterate the animals list, but use enumerate() so that you can access
+    # the index of each item
+
+    for index, animal in enumerate(ANIMALS):
+        if animal["id"] == id:
+            animal_index = index
+    # If the animal was found, use pop(int) to remove it from the list
+    if animal_index >= 0:
+        ANIMALS.pop(animal_index)
