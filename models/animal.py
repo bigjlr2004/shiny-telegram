d68f1d3aada15ee566011d1202c034c41d3049f7
John Roberts <bigjlr2004@yahoo.com>
2024-09-27 17:49:51 -0500

    Added classes to Animals, Locations, Employees, Customers

---
+++ models/animal.py
@@ -0,0 +1,12 @@
+class Animal:
+
+    # Class initializer. It has five custom parameters,  with the
+    # special self parameter that every method on a class
+    # needs as it's first parameter.
+    def __init__(self, id, name, breed, status, location_id, customer_id):
+        self.id = id
+        self.name = name
+        self.breed = breed
+        self.status = status
+        self.location_id = location_id
+        self.customer_id = customer_id
